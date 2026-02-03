03e48b37c680329314c8b2375050b26c08791a5f
Joseph <23200js@hvhs.school.kiwi>
2026-02-03 15:09:01 +1300

    sssss

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 alert("Congratulations!\nYou got the script running.")
+const button = document.getElementById("myButton");
+const outputParagraph = document.getElementById("output");
+
+button.addEventListener("click", function() {
+    alert("Hello");
+});
